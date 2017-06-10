33ae0d647074abd606ad00b657341229c972b720
Masaki Iwabuchi <ituki1059@gmail.com>
2017-06-10 15:24:42 +0900

    no message

---
+++ js/owl.carousel.custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,	
+		stagePadding: 75,
+		margin: 15,
+		lazyLoad: true,
+		autoHeight:true,
+	});
+	$( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
+});
